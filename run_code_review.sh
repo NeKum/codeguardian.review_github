8a6230db9571c7bae24a1a469bffc584abd4da3d
Oleg Nemna <nemnao@gmail.com>
2024-06-27 15:14:36 +0200

    GitHub action

--- run_code_review.sh
+++ run_code_review.sh
@@ -2,6 +2,6 @@
 
 URL="http://api.codeguardian.tech:8084/api/v1/review/github"
 
-curl -X POST $URL \
+curl -X PUT $URL \
 -H "Content-Type: application/json" \
 -d "{\"github_token\": \"$GITHUB_TOKEN\", \"github_event_path\": \"$GITHUB_EVENT_PATH\", \"key\": \"$API_KEY\"}"
