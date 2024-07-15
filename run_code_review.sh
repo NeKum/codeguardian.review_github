df69f0a003e1a698cab0e83b39668b43db623547
Oleg Nemna <nemnao@gmail.com>
2024-07-15 14:55:00 +0200

    fix

--- run_code_review.sh
+++ run_code_review.sh
@@ -2,6 +2,13 @@
 
 URL="https://api.codeguardian.tech/api/v1/review/github"
 
+# Чтение содержимого файла в переменную
+EVENT_DATA=$(cat "$GITHUB_EVENT_PATH")
+
+# Экранирование переменной для использования в JSON
+EVENT_DATA_ESCAPED=$(echo "$EVENT_DATA" | jq -Rsa .)
+
+# Отправка данных в API
 curl -X PUT $URL \
 -H "Content-Type: application/json" \
--d "{\"GITHUB_TOKEN\": \"$GITHUB_TOKEN\", \"GITHUB_EVENT_DATA\": \"$EVENT_DATA\", \"key\": \"$KEY\"}"
+-d "{\"GITHUB_TOKEN\": \"$GITHUB_TOKEN\", \"GITHUB_EVENT_DATA\": $EVENT_DATA_ESCAPED, \"key\": \"$KEY\"}"
